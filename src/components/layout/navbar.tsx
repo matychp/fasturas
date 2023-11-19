1af7c983b0552d3297082d45043d0afa1c88e960
Matias Lopez <matychp@gmail.com>
2023-11-19 15:39:38 -0300

    :lipstick: Use icon instead of emoji on the navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ModeToggle } from "~/components/layout/modeToggle";
 import { TypographyH1 } from "~/components/typography/h1";
 import { SessionButton } from "./session-button";
@@ -6,7 +7,7 @@ export const NavBar = () => {
   return (
     <div className="flex h-16 w-full items-center justify-between px-4">
       <div className="flex gap-2">
-        <TypographyH1>🥐</TypographyH1>
+        <Image src="/favicon.ico" alt="a croissant" width={48} height={48} />
         <TypographyH1 className="underline underline-offset-4">
           Fasturas
         </TypographyH1>
